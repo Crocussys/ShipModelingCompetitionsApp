6237462c688127e1548d5391f12f2c812d86ed87
Crocussys <egor.vladislavovich.nn@gmail.com>
2026-06-29 23:42:06 +0300

    fix config

--- src/config.py
+++ src/config.py
@@ -1,16 +1,40 @@
+import sys
+import os
 from pathlib import Path
 import configparser
 
 
-CONFIG_PATH = Path("src/config.ini")
+if getattr(sys, "frozen", False):
+    EXE_DIR = Path(sys.executable).parent
+else:
+    EXE_DIR = Path(__file__).resolve().parent
+
+CONFIG_PATH = EXE_DIR / "config.ini"
+APP_NAME = "ShipModelingCompetitionsApp"
 
 
 def get_database_path() -> Path:
-    if not CONFIG_PATH.exists():
-        raise FileNotFoundError("Файл config.ini не найден")
-
     config = configparser.ConfigParser()
+
+    if not CONFIG_PATH.exists():
+        appdata = (
+            Path(os.getenv("APPDATA"))
+            / APP_NAME
+        )
+        appdata.mkdir(parents=True, exist_ok=True)
+
+        default_db = appdata / "db.sqlite"
+
+        config["database"] = {
+            "path": str(default_db)
+        }
+
+        with open(CONFIG_PATH, "w", encoding="utf-8") as file:
+            config.write(file)
+
     config.read(CONFIG_PATH, encoding="utf-8")
 
-    db_path = config["database"]["path"]
-    return Path(db_path)
+    db_path = Path(config["database"]["path"])
+    db_path.parent.mkdir(parents=True, exist_ok=True)
+
+    return db_path
